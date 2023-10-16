e6f31563fbf8e782d42609afaca58a2a2ad1a9c6
NanaBanana14 <nananabananayaya@gmail.com>
2023-10-16 15:48:05 +0700

    Initialize automated testing implemented

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,70 @@
+import UrlParser from '../../routes/url-parser';
+import RestaurantSource from '../../data/restourantdb-source';
+import { createRestaurantDetailTemplate } from '../templates/template-creator';
+import LikeButtonPresenter from '../../utils/like-button-presenter';
+import FavoriteRestaurantIdb from '../../data/favorite-restaurant-idb';
+
+const DetailRestaurant = {
+  async render() {
+    return `
+    <div class="content-detail">
+      <div class="content__header">
+        <h2 class="content__heading">Restaurant Detail</h2>
+        <div class="buttons-container">
+          <div id="likeButtonContainer"></div>
+          <button class="back-button" id="back-button">Kembali</button>
+        </div>
+      </div>
+      <div id="loading-indicator" class="loading-indicator">
+        <p class="loading">Loading...</p>
+      </div>
+      <div id="restaurant" class="restaurant" style="display: none;"></div>
+    </div>
+    `;
+  },
+
+  async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurantContainer = document.querySelector('#restaurant');
+    const loadingIndicator = document.getElementById('loading-indicator');
+
+    loadingIndicator.style.display = 'block';
+
+    try {
+      const restaurant = await RestaurantSource.detailRestaurant(url.id);
+
+      restaurantContainer.style.display = 'block';
+      loadingIndicator.style.display = 'none';
+
+      restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant);
+
+      LikeButtonPresenter.init({
+        likeButtonContainer: document.querySelector('#likeButtonContainer'),
+        favoriteRestaurants: FavoriteRestaurantIdb,
+        restaurant: {
+          id: restaurant.id,
+          name: restaurant.name,
+          pictureId: restaurant.pictureId,
+          city: restaurant.city,
+          address: restaurant.address,
+          rating: restaurant.rating,
+        },
+      });
+    } catch (error) {
+      loadingIndicator.style.display = 'none';
+      restaurantContainer.innerHTML = 'An error occurred while fetching restaurant details.';
+    }
+
+    const backButton = document.getElementById('back-button');
+    backButton.addEventListener('click', () => {
+      window.history.back();
+    });
+
+    const addReviewButton = document.getElementById('add-review-button');
+    addReviewButton.addEventListener('click', () => {
+      window.location.hash = '/review';
+    });
+  },
+};
+
+export default DetailRestaurant;
---
+++ webpack.common.js
@@ -0,0 +1,136 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// eslint-disable-next-line
+const webpack = require('webpack');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
+const ImageminMozjpeg = require('imagemin-mozjpeg');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+// eslint-disable-next-line prefer-destructuring
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'src/scripts/index.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/templates/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: './sw.bundle.js',
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev/'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'therestaurantbbdb-api',
+          },
+        },
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev/list'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'thelistrestaurantdb-api',
+          },
+        },
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev/images/medium/<pictureId>'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'therestaurantbb-image-api',
+          },
+        },
+      ],
+    }),
+    new ImageminWebpackPlugin({
+      plugins: [
+        ImageminMozjpeg({
+          quality: 50,
+          progressive: true,
+        }),
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public'),
+          to: path.resolve(__dirname, 'dist'),
+          globOptions: {
+            // CopyWebpackPlugin mengabaikan berkas yang berada di dalam folder images
+            ignore: ['**/images/**'],
+          },
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles/style.css', // Nama file CSS hasil kompilasi
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+};
